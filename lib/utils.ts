a1e1643eee064e3697bd060f07cfdb8549794f84
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-16 13:24:13 -0400

    Add utility function to format numbers with two decimal places for pricing data

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,13 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(value: T) :T {
   return JSON.parse(JSON.stringify(value));
 }
+
+//utility function to format a number to have two decimal places
+//used for pricing data in schemas and avoid miscalculations from rounding errors etc
+export function formatNumberWithDecimal(num: number) : string {
+  const [int, decimal] = num.toString().split(',');
+  
+  const numberWithDecimal = decimal ? `${int}.${decimal.padEnd(2,'0')}` : `${int}.00`;
+
+  return numberWithDecimal;
+}
