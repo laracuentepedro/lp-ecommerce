ecf3fc1fc5ec82ed8adce922cc9c39602715cd0a
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-16 15:05:37 -0400

    Refactor product components to use Product type for improved type safety and add currency validation for price

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,6 +1,12 @@
 import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
+const currency = z
+  .string()
+  .refine(
+    (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
+    "Price must have exactly two decimal places (e.g., 49.99)"
+  );
 //Schema for inserting products
 export const insertProductSchema = z.object({
   name: z.string().min(3, "Name must be at least 3 character"),
@@ -12,10 +18,5 @@ export const insertProductSchema = z.object({
   images: z.array(z.string()).min(1, "Product must have at least 1 image"),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
-  price: z
-    .string()
-    .refine(
-      (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
-      "Price must have exactly two decimal places (e.g., 49.99)"
-    ),
+  price: currency
 });
