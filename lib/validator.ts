fbfd5e777aea0773c6dcd1ef61112baf3a8500c1
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-16 13:05:36 -0400

    Add schema validation for product insertion using Zod

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,1 +1,14 @@
-import { z } from 'zod';
+import { z } from "zod";
+
+//Schema for inserting products
+export const insertProductSchema = z.object({
+  name: z.string().min(3, "Name must be at least 3 character"),
+  slug: z.string().min(3, "Slug must be at least 3 character"),
+  category: z.string().min(3, "Category must be at least 3 character"),
+  brand: z.string().min(3, "Brand must be at least 3 character"),
+  description: z.string().min(3, "Description must be at least 3 character"),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, 'Product must have at least 1 image'),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+});
