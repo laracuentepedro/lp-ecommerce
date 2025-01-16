92c07d2b410dbcaf6b164406c31f7216c6d39129
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-16 10:01:27 -0400

    Fetch latest products for homepage and update ProductList component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
 const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
   return (
     <>
-      <ProductList data={sampleData.products} limit={4} title="Newest Arrivals" />
+      <ProductList data={latestProducts} limit={4} title="Newest Arrivals" />
     </>
   );
 };
