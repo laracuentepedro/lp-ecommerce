14136281597ca82df0b68fe45a2466483e28e764
Pedro Laracuente <laracuentepedro@gmail.com>
2025-01-17 14:28:38 -0400

    Add ProductDetailsPage component and Badge UI component for product display

---
+++ app/(root)/product/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import { getProductBySlug } from "@/lib/actions/product.actions";
+import { notFound } from "next/navigation";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import ProductPrice from "@/components/shared/product/product-price";
+
+const ProductDetailsPage = async (props: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await props.params;
+  const product = await getProductBySlug(slug);
+  if (!product) notFound();
+
+  return <>{product.name}</>;
+};
+
+export default ProductDetailsPage;
